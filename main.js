0bc4fdbf420dbbef0082a85a804416b2ff40dcf9
Raphael Capeto <raphaelcapeto@gmail.com>
2021-06-23 02:26:38 -0300

    Class-3

--- main.js
+++ main.js
@@ -31,3 +31,31 @@ navContainer.addEventListener('click', function(event) {
 
    document.getElementById(divId).scrollIntoView({ behavior: 'smooth' });
 });
+
+//Carousel - Swiper
+const swiper = new Swiper('.swiper-container', {
+   slidesPerView: 1,
+   pagination: {
+      el: '.swiper-pagination'
+   },
+   mousewheel: true,
+   keyboard: true
+ });
+
+//scrollReveal - Scroll Animado 
+const scrollReveal = ScrollReveal({
+   origin: 'top',
+   distance: '30px',
+   duration: 700,
+   reset: true
+});
+
+scrollReveal.reveal(
+   `#home .text, #home .image, 
+   #about .image, #about .text,
+   #services header, #services .card,
+   #testimonials header, #testimonials .testimonials,
+   #contact .text, #contact .links
+   `, 
+   { interval: 100 }
+);
